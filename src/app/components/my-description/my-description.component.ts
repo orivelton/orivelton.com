941d3f2b21ab4f190ce7e6c6bac8907cfbb83f0d
Orivelton Cesar Da Conceição <oriveltoncesar@MacBook-Air-de-Orivelton.local>
2018-05-30 11:57:08 -0300

    Add components description and social, add toogle class in translate component

---
+++ src/app/components/my-description/my-description.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-description',
+  templateUrl: './my-description.component.html',
+  styleUrls: ['./my-description.component.scss']
+})
+export class MyDescriptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
