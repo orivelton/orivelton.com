16b46cc4ee6f6a66929cfa52e702f45e4b408d2f
Orivelton <orivelton@Oriveltons-MBP.Home>
2020-04-28 10:21:18 +0100

    chore: new component

---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Logo from './Logo';
+
+const Nav = () => (
+  <nav>
+    <Logo />
+    <ul>
+      <li>
+        <a href="/">Contact</a>
+      </li>
+    </ul>
+  </nav>
+);
+
+export default Nav;
