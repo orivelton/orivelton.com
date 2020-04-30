4dac1102bde7b53ed5dab83a11c1b856ac93cbca
Orivelton <orivelton@Oriveltons-MBP.Home>
2020-04-30 07:45:10 +0100

    refactor: path

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
       summary: `Front End Developer`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `orivelton.com`,
+    siteUrl: `https://orivelton.com`,
     social: {
       twitter: `kylemathews`,
     },
