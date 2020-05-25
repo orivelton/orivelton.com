501db62d9229825f7ba085acd9a831a6e0ece652
Orivelton Cesar <orivelton@studiographene.com>
2020-05-25 17:16:49 +0100

    chore: added new post

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -13,23 +13,26 @@ const Blog = ({ data, location }) => {
         posts.map(({ node }) => {
           const title = node.frontmatter.title || node.fields.slug
           return (
-            <article key={node.fields.slug}>
-              <header>
-                <h3>
-                  <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-                    {title}
-                  </Link>
-                </h3>
-                <small>{node.frontmatter.date}</small>
-              </header>
-              <section>
-                <p
-                  dangerouslySetInnerHTML={{
-                    __html: node.frontmatter.description || node.excerpt,
-                  }}
-                  />
-              </section>
-            </article>
+            <>
+              <article key={node.fields.slug}>
+                <header>
+                  <h3>
+                    <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+                      {title}
+                    </Link>
+                  </h3>
+                  <small>{node.frontmatter.date}</small>
+                </header>
+                <section>
+                  <p
+                    dangerouslySetInnerHTML={{
+                      __html: node.frontmatter.description || node.excerpt,
+                    }}
+                    />
+                </section>
+              </article>
+              <br /> <hr /> <br />
+            </>
           )
         })
       }
