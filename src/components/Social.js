65df7fe9f18ce82d2b0d833c195ba4c58d62307f
Orivelton Cesar <orivelton10@gmail.com>
2020-07-10 15:02:21 +0100

    refactor: Social names

--- src/components/Social.js
+++ src/components/Social.js
@@ -1,38 +1,20 @@
 import React from 'react';
 import '../assets/scss/Social.scss';
 
-const Social = () => {
-  const socialNames = [
+import { socialNames } from '../data/social';
+
+const Social = () => (
+  <ul className="social">
     {
-      name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/orivelton/',
-      icon: 'linkedin'
-    },
-    {
-      name: 'GitHub',
-      url: 'https://github.com/orivelton',
-      icon: 'github'
-    },
-    {
-      name: 'Twitter',
-      url: 'https://twitter.com/orivelton_cesar',
-      icon: 'twitter'
+      socialNames.map(({ name, url, icon }) => (
+        <li key={name}>
+          <a href={url} className={`icon icon-${icon}`} target="_blank" title={`Got to ${name}`} rel="noopener noreferrer">
+            <span className="icon__name">{ name }</span>
+          </a>
+        </li>
+      ))
     }
-  ];
-  
-  return (
-    <ul className="social">
-      {
-        socialNames.map(({ name, url, icon }) => (
-          <li key={name}>
-            <a href={url} className={`icon icon-${icon}`} target="_blank" title={`Got to ${name}`} rel="noopener noreferrer">
-              <span className="icon__name">{ name }</span>
-            </a>
-          </li>
-        ))
-      }
-    </ul>
-  )
-};
+  </ul>
+);
 
 export default Social;
--- src/components/layout.js
+++ src/components/layout.js
@@ -2,18 +2,16 @@ import React from 'react';
 import Nav from './Nav';
 import Footer from './Footer';
 
-const Layout = ({ children }) => {
-  return (
-    <>
-      <header>
-        <Nav />
-      </header>
-      <div className="content">
-        <main>{children}</main>
-      </div>
-      <Footer />
-    </>
-  )
-}
+const Layout = ({ children }) => (
+  <>
+    <header>
+      <Nav />
+    </header>
+    <div className="content">
+      <main>{children}</main>
+    </div>
+    <Footer />
+  </>
+);
 
 export default Layout
