41e5b947ce113b33c8e982609ee41cbe0758baae
Orivelton Cesar <orivelton10@gmail.com>
2020-07-18 09:55:09 +0100

    fix: Removed error with meta type

--- src/components/Name.js
+++ src/components/Name.js
@@ -16,7 +16,7 @@ const Name = () => {
         I work as a Front End but I have already ventured into the logistics and administrative area but found myself even within the codes. I've worked on big projects with many challenges and always been in love with open source projects. 
       </h3>
       <Social />
-      <SEO description="Front End Developer" title="Orivelton Cesar"/>
+      <SEO description="Front End Developer" title="Orivelton Cesar" />
     </div>
   )
 };
