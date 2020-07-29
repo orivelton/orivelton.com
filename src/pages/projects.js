d0554405bf1f40fb5026f00868db3c6f4017f8a2
Orivelton Cesar <orivelton10@gmail.com>
2020-07-29 17:15:57 +0100

    chore: Added projects

---
+++ src/pages/projects.js
@@ -0,0 +1,58 @@
+
+import React, { useState, useEffect } from 'react';
+import { graphql } from 'gatsby';
+import axios from 'axios';
+
+import '../assets/scss/Projects.scss';
+
+import Layout from '../components/Layout';
+
+const Projects = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata.title;
+
+  const [repos, setRepos] = useState([]);
+ 
+  useEffect( () => {
+    const result = async () => {
+      const { data: repositories } = await axios('https://api.github.com/users/orivelton/repos?per_page=12');
+      console.log(repositories);
+      setRepos(repositories);
+    }
+
+    result();
+  }, []);
+
+
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <ul className="github-cards">
+        {
+          repos.map(({ id, html_url: htmlUrl, name, description, language }) => (
+            <li key={id}>
+              <a href={htmlUrl} className="github-card" target="_blank">
+                <h3>{name}</h3>
+                <p>{description}</p>
+                <span className="github-card__meta">
+                  <span className="github-card__language-icon">●</span> {language}
+                </span>
+              </a>
+            </li>
+          ))
+        }
+      </ul>
+    </Layout>
+  )
+}
+
+export default Projects;
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`;
