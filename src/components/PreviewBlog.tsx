452b1d5cd76f92b492470a61ffd171acc7c8ee36
Orivelton Cesar <orivelton10@gmail.com>
2021-02-21 13:02:05 +0000

    chore: Blog at Home

--- src/components/PreviewBlog.tsx
+++ src/components/PreviewBlog.tsx
@@ -1,14 +1,5 @@
-import { Box, Container, Heading, SimpleGrid, Icon, Text, Stack, HStack, VStack } from '@chakra-ui/react';
-import { BsFileRichtext } from 'react-icons/bs';
-import { Post } from '../interfaces/Post';
-
-const features = Array.apply(null, Array(8)).map(function (x, i) {
-  return {
-    id: i,
-    title: 'Lorem ipsum dolor sit amet',
-    text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam.',
-  };
-});
+import { Box, Container, Heading, SimpleGrid, Text, Stack, HStack, VStack, Flex } from '@chakra-ui/react'
+import { Post } from '../interfaces/Post'
 
 export default function PreviewBlog({ posts }: any) {
   return (
@@ -18,20 +9,17 @@ export default function PreviewBlog({ posts }: any) {
       </Stack>
 
       <Container maxW={'6xl'} mt={10}>
-        <SimpleGrid columns={{ base: 1, md: 2, lg: 4 }} spacing={10}>
+        <Flex columns={{ base: 1, md: 2, lg: 4 }} spacing={10}>
           {posts.length && posts.map(({ id, title, content, updatedAt } :Post) => (
-            <HStack key={id} align={'top'}>
-              <Box px={2}>
-                <Icon as={BsFileRichtext} />
-              </Box>
+            <HStack key={id}>
               <VStack align={'start'}>
                 <Text fontWeight={600}>{title}</Text>
-                <Text>{content.substring(0, 200)}</Text>
                 <Text>{updatedAt}</Text>
+                <Text>{content.substring(0, 230)}...</Text>
               </VStack>
             </HStack>
           ))}
-        </SimpleGrid>
+        </Flex>
       </Container>
     </Box>
   )
