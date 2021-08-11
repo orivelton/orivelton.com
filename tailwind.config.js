1963e75ad3f5b7c8d9817ab58cd8c2a048b6f5d4
Orivelton Cesar <orivelton10@gmail.com>
2021-08-11 18:21:14 +0100

    Chore: Project organization

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const { spacing } = require('tailwindcss/defaultTheme');
 
 module.exports = {
   mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
