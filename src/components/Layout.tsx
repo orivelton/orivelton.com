f283bc73778c38a5a04e0a130091c01b729c8cdd
Orivelton Cesar <orivelton10@gmail.com>
2021-08-21 13:55:58 +0100

    chore: Footer improvements

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,6 +2,7 @@ import { MetaProps } from '../types/layout';
 import Head from './Head';
 import Navigation from './Navigation';
 import ThemeSwitch from './ThemeSwitch';
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai'
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -27,8 +28,23 @@ const Layout = ({ children, customMeta }: LayoutProps): JSX.Element => {
         <div className="max-w-5xl px-8 py-4 mx-auto">{children}</div>
       </main>
       <footer className="py-8">
-        <div className="max-w-5xl px-8 mx-auto">
-          Orivelton César - { new Date().getFullYear() }
+        <div className="max-w-5xl px-8 mx-auto ">
+          <p className="flex flex-row justify-center">
+            Orivelton César - { new Date().getFullYear() }
+          </p>
+          <ul className="flex flex-row justify-center">
+            <li>
+              <a href="https://github.com/orivelton" title="Go to my profile on Github" rel="noopener noreferrer nofollow" target="_blank">
+                <AiFillGithub />
+              </a>
+            </li>
+            <li className="ml-4">
+              <a href="https://www.linkedin.com/in/orivelton/" title="Go to my profile on LikedIn" rel="noopener noreferrer nofollow" target="_blank">
+                <AiFillLinkedin />
+              </a>
+            </li>
+          </ul>
+
         </div>
       </footer>
     </>
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -8,7 +8,7 @@ export const About = (): JSX.Element => {
       }}
     >
       <h1>About me</h1>
-      <p>{`I work as a Front End but I have already ventured into the logistics and administrative area but found myself even within the codes. I've worked on big projects with many challenges and always been in love with open source projects`}</p>
+      <h2 className="text-base font-normal">{`I've been work as Front End but I have already ventured into the logistics and administrative area but found myself even within the codes. I've worked on big projects with many challenges and always been in love with open source projects`}</h2>
     </Layout>
   )
 }
