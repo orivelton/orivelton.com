74c403f9bd802b46ad81d29d36d50e81f1c39715
Orivelton Cesar <orivelton10@gmail.com>
2021-09-20 09:23:08 +0100

    chore:

--- src/examples/__tests__/Avatar.test.js
+++ src/examples/__tests__/Avatar.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
-import Avatar from '../Avatar'
+import Avatar from './Avatar'
+
 test("Render Avatar component", () => {
     render(<Avatar />)
     screen.logTestingPlaygroundURL()
