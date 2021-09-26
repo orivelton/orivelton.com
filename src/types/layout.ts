6ddb6edcfe409eeb99c7be8ecb319deb850378cb
Orivelton Cesar <orivelton10@gmail.com>
2021-09-26 11:38:26 +0100

    refactor: Clean code

--- src/types/layout.ts
+++ src/types/layout.ts
@@ -2,8 +2,5 @@ import { PostType } from './post';
 
 export interface MetaProps
   extends Pick<PostType, 'date' | 'description' | 'image' | 'title'> {
-  /**
-   * For the meta tag `og:type`
-   */
   type?: string;
 }
--- src/types/post.ts
+++ src/types/post.ts
@@ -4,4 +4,4 @@ export type PostType = {
   image?: string;
   slug: string;
   title: string;
-};
+}
