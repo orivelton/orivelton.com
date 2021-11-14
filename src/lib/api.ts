85d75bcaf1b697d492ef3a5d9bb0efd2739cc885
Orivelton Cesar <orivelton10@gmail.com>
2021-11-14 22:15:56 +0000

    chore: Drafts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -31,7 +31,7 @@ export function getPostBySlug(slug: string, fields: string[] = []): PostItems {
 export function getAllPosts(fields: string[] = []): PostItems[] {
   const slugs = getPostSlugs()
   const posts = slugs
-    .map((slug) => getPostBySlug(slug, fields))
+    .map(slug => getPostBySlug(slug, fields))
     .sort((post1, post2) => (post1.date > post2.date ? -1 : 1))
   return posts
 }
--- src/utils/mdxUtils.ts
+++ src/utils/mdxUtils.ts
@@ -3,4 +3,6 @@ import path from 'path'
 
 export const POSTS_PATH = path.join(process.cwd(), 'src/posts')
 
-export const postFilePaths = fs.readdirSync(POSTS_PATH).filter((path) => /\.mdx?$/.test(path))
+export const postFilePaths = fs.readdirSync(POSTS_PATH).filter(path => /\.mdx?$/.test(path))
+
+console.log(postFilePaths)
