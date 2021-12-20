58b751b35661476aa30d1bb448e5f67569a7bdac
Orivelton Cesar <orivelton10@gmail.com>
2021-12-20 19:27:52 +0000

    fix: escaped caracter

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -5,7 +5,7 @@ export const About = (): JSX.Element => {
     <Layout customMeta={{ title: 'About - Orivelton César' }}>
       <h1>About me</h1>
       <h2 className="text-base font-normal">
-        I've been work as Front End but I have already ventured into the logistics and administrative area but found myself even within the codes. I've worked on big projects with many challenges and always been in love with open source projects
+        {`I've been work as Front End but I have already ventured into the logistics and administrative area but found myself even within the codes. I've worked on big projects with many challenges and always been in love with open source projects`}
       </h2>
     </Layout>
   )
